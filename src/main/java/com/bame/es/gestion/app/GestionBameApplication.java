8690482fd2ad57ae2c9212767647a6be645fc885
Jorge <jorge10_stp@hotmail.com>
2020-04-02 19:51:22 +0200

    Se implementa en el Main la interfaz y en su metodo se borra todo el directorio de archivos y se inicializa el folder

--- src/main/java/com/bame/es/gestion/app/GestionBameApplication.java
+++ src/main/java/com/bame/es/gestion/app/GestionBameApplication.java
@@ -1,13 +1,27 @@
 package com.bame.es.gestion.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GestionBameApplication {
+import com.bame.es.gestion.app.models.service.impl.IUploadFileService;
 
+@SpringBootApplication
+public class GestionBameApplication implements CommandLineRunner {
+
+	@Autowired
+	private IUploadFileService uploadService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestionBameApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		uploadService.deleteAll();
+		uploadService.init();
 	}
 
 }
--- src/main/java/com/bame/es/gestion/app/models/service/IUploadFileService.java
+++ src/main/java/com/bame/es/gestion/app/models/service/IUploadFileService.java
@@ -7,12 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
-
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+
+@Service
 public class IUploadFileService implements com.bame.es.gestion.app.models.service.impl.IUploadFileService {
 
 	private static final String FOLDER="uploads";
