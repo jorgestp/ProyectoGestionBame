30d479242cdbc597978ce586b90a62e375b71acd
Jorge <jorge10_stp@hotmail.com>
2020-04-02 19:38:34 +0200

    Se implementa el resto de metodos de UploadServiceImpl

--- src/main/java/com/bame/es/gestion/app/models/service/IUploadFileService.java
+++ src/main/java/com/bame/es/gestion/app/models/service/IUploadFileService.java
@@ -1,11 +1,14 @@
 package com.bame.es.gestion.app.models.service;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,8 +19,18 @@ public class IUploadFileService implements com.bame.es.gestion.app.models.servic
 	@Override
 	public Resource load(String filename) throws MalformedURLException {
 		
+		Path pathFoto = getPath(filename);
+
+		Resource recurso = null;
 		
-		return null;
+		recurso = new UrlResource(pathFoto.toUri());
+
+		if (!recurso.exists() || !recurso.isReadable()) {
+
+			throw new RuntimeException("Error, no se puede cargar la imagen" + pathFoto.toString());
+		}
+
+		return recurso;
 	}
 
 	@Override
@@ -28,7 +41,24 @@ public class IUploadFileService implements com.bame.es.gestion.app.models.servic
 
 	@Override
 	public boolean delete(String filename) {
-		// TODO Auto-generated method stub
+		
+		//obtenemos la ruta absoluta
+		Path path = getPath(filename);
+		
+		
+		
+		//Obtenemos el file
+		File archivo = path.toFile();
+		
+		//eliminamos el archivo. Preguntamos primero si el archivo existe y se puede leer
+		if(archivo.exists() && archivo.canRead()) {
+			
+			//SI se elimina, mandamo un flash
+			if(archivo.delete()) {
+				
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -50,6 +80,12 @@ public class IUploadFileService implements com.bame.es.gestion.app.models.servic
 	public byte[] mostrar(String filename) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Path getPath (String filename) {
+		
+		//Devolvemos la ruta absoluta donde se encuentra FOLDER, concatenadondo el nombre del archivo
+		return Paths.get(FOLDER).resolve(filename).toAbsolutePath();
 	}
 
 }
