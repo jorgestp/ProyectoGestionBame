3c466d7903332b3615cd7cb9986cadc7b0b402a3
Jorge <jorge10_stp@hotmail.com>
2020-04-08 17:56:01 +0200

    Se muesta el formulario de la nueva voz.
    Al pulsar en nueva voz en la vista del detalle de la marcha,
    se busca en el controller de la voz la marcha por el id pasado,
    se crea una voz nueva, y a esa voz se le asigna la propia marcha.
    Esta voz es pasada al formulario del metodo crear

--- src/main/java/com/bame/es/gestion/app/controllers/VozController.java
+++ src/main/java/com/bame/es/gestion/app/controllers/VozController.java
@@ -1,15 +1,39 @@
 package com.bame.es.gestion.app.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bame.es.gestion.app.models.entity.Marcha;
+import com.bame.es.gestion.app.models.entity.Voz;
+import com.bame.es.gestion.app.models.service.impl.IMarchaService;
 
 @Controller
 @RequestMapping(value = "/voz")
 public class VozController {
+	
+	@Autowired
+	IMarchaService marchaService;
 
 	@RequestMapping(value = "/form/{id}")
-	public String crear(@PathVariable(name = "id") Long id) {
+	public String crear(@PathVariable(name = "id") Long id, Map<String, Object> model,RedirectAttributes flash) {
+		
+		Marcha marcha = marchaService.findById(id);
+		
+		if(marcha == null) {
+			
+			flash.addFlashAttribute("error", "El cliente no existe");
+			return "redirect:/repertorio/lista";
+		}
+		
+		Voz nuevaVoz = new Voz();
+		nuevaVoz.setMarcha(marcha);
+		model.put("titulo", "Nueva voz para marcha " + marcha.getNombre());
+		model.put("voz", nuevaVoz);
 		
 		return "voz/formVoz";
 	}
