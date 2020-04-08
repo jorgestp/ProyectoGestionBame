09279e9df4ec781b326c51491c66c5d1e3245fec
Jorge <jorge10_stp@hotmail.com>
2020-04-08 17:20:26 +0200

    Se crea el controlador vacio de la Voz

---
+++ src/main/java/com/bame/es/gestion/app/controllers/VozController.java
@@ -0,0 +1,18 @@
+package com.bame.es.gestion.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/voz")
+public class VozController {
+
+	@RequestMapping(value = "/form/{id}")
+	public String crear(@PathVariable(name = "id") Long id) {
+		
+		return null;
+	}
+
+
+}
