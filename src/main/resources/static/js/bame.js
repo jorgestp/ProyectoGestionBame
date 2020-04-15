a4d9017eb656f770976a872ec79aca5d290ca6ce
Jorge <jorge10_stp@hotmail.com>
2020-04-15 19:07:26 +0200

    Funciona la creacion de tabla dinamica

--- src/main/resources/static/js/bame.js
+++ src/main/resources/static/js/bame.js
@@ -3,8 +3,40 @@
  */
 
 $(document).ready(function() {
-			
+	
+	var id ="";
+	var nombre="";
 	$("#id_seleccion").on('change', '#seleccion', function(event) {
 	     alert("EL VALOR SELECCIONADO ES "+ $("#seleccion").val());
+	     
+			$.get( "/prestamo/cargar-productos/" + $("#seleccion").val(), function( data ) {
+				$("#seleccion").val("");
+				  //alert( "Load was performed." );
+				  //alert(JSON.stringify(data));
+				  
+				  //alert("VALOR DE DATA NOMBRE ES " + data["nombre"]);
+				  //alert("VALOR DE DATA NOMBRE ES " + data["id"]);
+
+				  id=data["id"];
+				  nombre=data["nombre"];
+				  alert(id);
+				  alert(nombre);
+				  
+					var linea = $("#plantillaItemsMaterial").html();
+					
+					alert("VALOR DE LA LINEA " + linea)
+					
+					linea = linea.replace(/{ID}/g, id);
+					linea = linea.replace(/{NOMBRE}/g, nombre);
+					
+					alert("VALOR DE LA LINEA " + linea);
+
+					$("#cargarItemProductos tbody").append(linea);
+
+				});
+			
+
 	});
+	
+
 });
