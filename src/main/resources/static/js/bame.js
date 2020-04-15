4eb9edf1404793d57f4d92d3fd4b63e93aee51a8
Jorge <jorge10_stp@hotmail.com>
2020-04-15 17:55:08 +0200

    Se recoge mediante JQuery el value de la opcion seleccionada

--- src/main/resources/static/js/bame.js
+++ src/main/resources/static/js/bame.js
@@ -4,5 +4,7 @@
 
 $(document).ready(function() {
 			
-	alert("HOLA");
+	$("#id_seleccion").on('change', '#seleccion', function(event) {
+	     alert("EL VALOR SELECCIONADO ES "+ $("#seleccion").val());
+	});
 });
